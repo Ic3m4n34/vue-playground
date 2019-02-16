fbaece60c07634906b6d9e2d18c6708033f920cc
Nico Meyer <n.meyer89@gmx.de>
2019-02-16 10:18:33 +0100

    Initial commit

---
+++ src/components/another-component.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="another-component"></div>
+</template>
+
+<script>
+export default {
+  name: 'AnotherCOmponent',
+  data() {
+    return {
+      data: 'Another Components Data'
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.another-component {
+  background: red;
+}
+</style>
---
+++ src/components/child-component.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="child-component"><slot :useMe="data" /></div>
+</template>
+
+<script>
+import AnotherComponent from '@/components/another-component';
+
+export default {
+  name: 'ChildComponent',
+  components: {
+    AnotherComponent
+  },
+  data() {
+    return {
+      data: 'Child Components Data'
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.child-component {
+  background: grey;
+}
+</style>
